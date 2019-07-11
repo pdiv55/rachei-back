156a9e0201dead946bfa4035c04addcb9b2ed9b0
pdiv55 <paul.divet5@gmail.com>
2019-07-11 20:37:16 -0300

    mudança de cookie-session para express-session

--- rachei/app.js
+++ rachei/app.js
@@ -1,57 +1,62 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 const passport = require("passport");
-const bodyParser = require('body-parser');
-const cors = require('cors')
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const groupsRouter = require('./routes/groups');
-const expensesRouter = require('./routes/expenses');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const groupsRouter = require("./routes/groups");
+const expensesRouter = require("./routes/expenses");
 
 const app = express();
 
 require("dotenv").config();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/groups', groupsRouter);
-app.use('/expenses', expensesRouter);
-
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1', 'key2'],
-
-  // Cookie Options
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+​
+app.use(session({
+ secret: "key1",
+ resave: true,
+ saveUninitialized: true,
+ cookie: { maxAge: 600000 },
+ rolling: true,
+ store: new MongoStore({
+  mongooseConnection: mongoose.connection,
+  ttl: 24 * 60 * 60
+ })
 }));
 
-require('./config/auth')(passport);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/groups", groupsRouter);
+app.use("/expenses", expensesRouter);
+
+require("./config/auth")(passport);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}, (error) => {
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, error => {
   if (error) {
-    console.log('Não consegui conectar');
+    console.log("Não consegui conectar");
   } else {
-    console.log('CONECTAMOS');
+    console.log("CONECTAMOS");
   }
 });
 
@@ -64,7 +69,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
